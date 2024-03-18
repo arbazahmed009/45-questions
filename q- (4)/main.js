ff654d91b043bd2ada4106968f493ea30a6ba2de
Arbaz Ahmed Khan <159795411+arbazahmed009@users.noreply.github.com>
2024-03-18 05:47:51 +0500

    Add files via upload

---
+++ q- (4)/main.js
@@ -0,0 +1,3 @@
+var qoute = "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.";
+var author = "Martin Luther King Jr";
+console.log("".concat(author, " once said ").concat(qoute));
---
+++ q- (4)/main.ts
@@ -0,0 +1,3 @@
+let qoute: string= "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.";
+let author: string= "Martin Luther King Jr";
+console.log(`${author} once said ${qoute}`);
